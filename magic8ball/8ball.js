3399fc2433b8eddcf005f6abd1604df6d9cc087b
LisaHernandez63 <lisaleesa63@gmail.com>
2018-03-26 21:08:37 -0400

    Add files via upload

--- magic8ball/8ball.js
+++ magic8ball/8ball.js
@@ -2,7 +2,7 @@
 var getAnswer = function() {
   var answers = ["Computer says no.", "Maybe", "Definitely","Yeah Mon", "Yup", "Not in a million years"];
   return answers[Math.floor(Math.random() * 6)]
-  
+
 }
 
 var askQuestion = function() {
